a3335e48a248d0d1b808a1a83928da9d1b79bc86
David G Chaves <davidgchaves@gmail.com>
2018-11-30 17:38:47 +0100

    Add log in capabilities

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,7 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
+import firebase from "firebase/app";
 import AddFishForm from "./AddFishForm";
 import EditFishForm from "./EditFishForm";
+import Login from "./Login";
+import base, { firebaseApp } from "../base";
 
 class Inventory extends React.Component {
   static propTypes = {
@@ -9,10 +12,56 @@ class Inventory extends React.Component {
     updateFish: PropTypes.func.isRequired,
     deleteFish: PropTypes.func.isRequired,
     addFish: PropTypes.func.isRequired,
-    loadSampleFishes: PropTypes.func.isRequired
+    loadSampleFishes: PropTypes.func.isRequired,
+    storeId: PropTypes.string.isRequired
+  };
+
+  state = {
+    currentUser: null,
+    shopOwner: null
+  };
+
+  authHandler = async authData => {
+    // 1. Look up the current store in the Firebase Database
+    const store = await base.fetch(this.props.storeId, { context: this });
+    // 2. Claim it if there's no previous owner
+    if (!store.owner) {
+      await base.post(`${this.props.storeId}/owner`, {
+        data: authData.user.uid
+      });
+    }
+    // 3. Set the state of <Inventory> to reflect the current user
+
+    this.setState({
+      currentUser: authData.user.uid,
+      shopOwner: store.owner || authData.user.uid
+    });
+  };
+
+  authenticate = provider => {
+    // Right now it's only `firebase.auth.GithubAuthProvider()`
+    const authProvider = new firebase.auth[`${provider}AuthProvider`]();
+    firebaseApp
+      .auth()
+      .signInWithPopup(authProvider)
+      .then(this.authHandler);
   };
 
   render() {
+    const amINotLoggedIn = !this.state.currentUser;
+    if (amINotLoggedIn) {
+      return <Login authenticate={this.authenticate} />;
+    }
+
+    const amINotTheOwner = this.state.currentUser !== this.state.shopOwner;
+    if (amINotTheOwner) {
+      return (
+        <div>
+          <p>Sorry you are not the owner!</p>
+        </div>
+      );
+    }
+
     return (
       <div className="inventory">
         <h2>Inventory</h2>
